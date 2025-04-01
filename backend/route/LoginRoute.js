c85d50bc043ff0a0a5d1d691e40eeef9f8b8854f
Jishnu M <mjihnu990@gmail.com>
2025-04-01 12:52:41 +0530

    add closing cash calculation and update logic

--- backend/route/LoginRoute.js
+++ backend/route/LoginRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { Login, SignUp } from '../controllers/LoginAndSignup.js';
 import { CreatePayment, GetPayment } from '../controllers/TransactionController.js';
+import { CloseController } from '../controllers/CloseController.js';
 
 const router = express.Router();
 
-router.post('/signin', SignUp).post('/login', Login).post('/createPayment', CreatePayment).get('/Getpayment', GetPayment)
+router.post('/signin', SignUp).post('/login', Login).post('/createPayment', CreatePayment).get('/Getpayment', GetPayment).post('/saveCashBank', CloseController)
 
 export default router;
